df94a803a51b04dbfd766ed2490932b36bf1c8e0
Николай Лапицкий <kolyanlapitskiy007@gmail.com>
2024-11-16 04:12:41 +0300

    До сих пор пробую исправить отображение(((

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import { useEffect } from 'react';
 import {useTelegram} from './hooks/useTelegram'
+import {Route, Routes } from 'react-router-dom';
 import Menu from './components/Menu/Menu';
 import Profile from './components/Profile/Profile';
 import Settings from './components/Settings/Settings';
 import Navbar from './components/Navbar/Navbar';
-import './App.css'
+
 
 // const API_BASE_URL = 'https://effortless-blancmange-d0fc57.netlify.app';
 
@@ -17,19 +18,27 @@ function App() {
         tg.ready();
     }, [tg])
     return (
-        <Router>
           <div className="app">
             <Routes>
+              <Route index element={<Menu />} />
               <Route path="/menu" element={<Menu />} />
               <Route path="/profile" element={<Profile />} />
               <Route path="/settings" element={<Settings />} />
-              <Route path="/" element={<Menu />} />
             </Routes>
             <Navbar />
           </div>
-        </Router>
+              //   <Router>
+              //   <div className="app">
+              //     <Routes>
+              //       <Route index element={<Menu />} />
+              //       <Route path="/menu" element={<Menu />} />
+              //       <Route path="/profile" element={<Profile />} />
+              //       <Route path="/settings" element={<Settings />} />
+              //     </Routes>
+              //     <Navbar />
+              //   </div>
+              // </Router>
     );
-
 }
   
 export default App;
