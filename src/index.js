faaf4dbc252f15c710f1c31fb74fa84b7e2742bf
Николай Лапицкий <kolyanlapitskiy007@gmail.com>
2024-11-18 07:27:36 +0300

    добавил кнопку backButton в index.js для изображения на каждой странице

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter as Router} from "react-router-dom";
 import './App.css';
+import BackButton from './components/BackButton/BackButton';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <React.StrictMode>
       <Router>
+          <BackButton/>
           <App />
       </Router>
   </React.StrictMode>
