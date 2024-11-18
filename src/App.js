dec12e07c6648c117143e3f4733e4138415527b1
Николай Лапицкий <kolyanlapitskiy007@gmail.com>
2024-11-18 07:21:19 +0300

    добавил небольшие заметки в app.js для будущей авторизации админа и работы с сайтом

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { TelegramWebApp } from "react-telegram-webapp";
+import { Provider } from "use-http";
 import React, { useEffect } from 'react';
 // import { useTelegram } from 'telegram-web-app';
 import {useTelegram} from './hooks/useTelegram'
@@ -11,22 +12,49 @@ import Settings from './components/Settings/Settings';
 import BackButton from './components/BackButton/BackButton';
 import Cart from './components/Cart/Cart';
 import ProductPage from './components/ProductPage/ProductPage';
+// import {
+//   UserContext as Context,
+//   useUserContext as useContext,
+// } from "@context/";
 
 
 // const API_BASE_URL = 'https://effortless-blancmange-d0fc57.netlify.app';
-// async function validateHash() {
-//   return true;
-// }
+
+
+async function validateHash() {
+  return true;
+}
 
 
 function App() {
+  // const dataUser = useContext();
+
+  // const options = {
+  //   cachePolicy: "no-cache",
+  //   interceptors: {
+  //     request: async ({ options }) => {
+  //       options.headers = {
+  //         "Content-Type": "application/json",
+  //         Authorization: "JWT " + localStorage.getItem("token"),
+  //       };
+  //       return options;
+  //     },
+  //     response: (props) => {
+  //       if (props.response.status === 401) {
+  //         dataUser.data.setIsUser(false);
+  //       }
+  //       return props.response;
+  //     },
+  //   },
+  // };
     const {tg} = useTelegram();
 
     useEffect(() => {
         tg.ready();
     }, [tg])
     return (
-      <TelegramWebApp>
+      <TelegramWebApp validateHash={validateHash}>
+        <Provider>
             <Routes>
               <Route index element={<Menu />} />
               <Route path="/product/:id" element={<ProductPage  />} />
@@ -34,6 +62,7 @@ function App() {
               <Route path="/settings" element={<Settings />} />
               <Route path="/cart" element={<Cart />} />
             </Routes>
+        </Provider>
       </TelegramWebApp>    
     );
 }
