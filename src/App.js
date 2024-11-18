13f7a3ed4a84747e4fd8a811f69b3e95f3e574ad
Николай Лапицкий <kolyanlapitskiy007@gmail.com>
2024-11-18 07:09:48 +0300

    обновил маршрутизацию всего приложения.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
+import { TelegramWebApp } from "react-telegram-webapp";
 import React, { useEffect } from 'react';
 // import { useTelegram } from 'telegram-web-app';
 import {useTelegram} from './hooks/useTelegram'
-import {Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Menu from './components/Menu/Menu';
 import Profile from './components/Profile/Profile';
 import Settings from './components/Settings/Settings';
 // import Navbar from './components/Navbar/Navbar';
 import BackButton from './components/BackButton/BackButton';
+import Cart from './components/Cart/Cart';
+import ProductPage from './components/ProductPage/ProductPage';
 
 
 // const API_BASE_URL = 'https://effortless-blancmange-d0fc57.netlify.app';
+// async function validateHash() {
+//   return true;
+// }
 
 
 function App() {
@@ -20,15 +26,15 @@ function App() {
         tg.ready();
     }, [tg])
     return (
-          <div className="app">
-            <BackButton />
+      <TelegramWebApp>
             <Routes>
               <Route index element={<Menu />} />
-              <Route path="/menu" element={<Menu />} />
+              <Route path="/product/:id" element={<ProductPage  />} />
               <Route path="/profile" element={<Profile />} />
               <Route path="/settings" element={<Settings />} />
+              <Route path="/cart" element={<Cart />} />
             </Routes>
-          </div>
+      </TelegramWebApp>    
     );
 }
 
