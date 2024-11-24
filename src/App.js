6a77c60d18bc717d9aa8b36b49e71ec852039716
Николай Лапицкий <kolyanlapitskiy007@gmail.com>
2024-11-24 05:08:40 +0300

    убрал лишний код с backutton

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import './App.css';
 import { TelegramWebApp } from "react-telegram-webapp";
-import React, { useState, useEffect } from 'react';
-// import { useTelegram } from 'telegram-web-app';
+import React, { useEffect } from 'react';
 import {useTelegram} from './hooks/useTelegram'
 import {Route, Routes } from 'react-router-dom';
 import Menu from './components/Menu/Menu';
 import Profile from './components/Profile/Profile';
 import Settings from './components/Settings/Settings';
-// import Navbar from './components/Navbar/Navbar';
 import BackButton from './components/BackButton/BackButton';
 import Cart from './components/Cart/Cart';
 import ProductPage from './components/ProductPage/ProductPage';
 import { useNavigate } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
-// import {
-//   UserContext as Context,
-//   useUserContext as useContext,
-// } from "@context/";
 
 
 // const API_BASE_URL = 'https://effortless-blancmange-d0fc57.netlify.app';
@@ -30,39 +24,19 @@ async function validateHash() {
 function App() {
 
   const navigate = useNavigate();
-  // const dataUser = useContext();
-
-  // const options = {
-  //   cachePolicy: "no-cache",
-  //   interceptors: {
-  //     request: async ({ options }) => {
-  //       options.headers = {
-  //         "Content-Type": "application/json",
-  //         Authorization: "JWT " + localStorage.getItem("token"),
-  //       };
-  //       return options;
-  //     },
-  //     response: (props) => {
-  //       if (props.response.status === 401) {
-  //         dataUser.data.setIsUser(false);
-  //       }
-  //       return props.response;
-  //     },
-  //   },
-  // };
     const {tg} = useTelegram();
 
-    const backButton = Telegram.WebApp.BackButton;
+    // const backButton = Telegram.WebApp.BackButton;
 
-    if (window.location.search && window.location.pathname !== '/') {
-      backButton.show();
-    } else {
-      backButton.hide(); 
-    }
+    // if (window.location.search && window.location.pathname !== '/') {
+    //   backButton.show();
+    // } else {
+    //   backButton.hide(); 
+    // }
 
-    backButton.onClick(() => {
-      navigate(-1);
-    });
+    // backButton.onClick(() => {
+    //   navigate(-1);
+    // });
 
     useEffect(() => {
       if (tg) {
